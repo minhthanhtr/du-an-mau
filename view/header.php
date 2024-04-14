b237063231aa26f9b1a6de532b20eb045566c002
minhthanhtr <minhthanhtr.wk@gmail.com>
2024-04-14 13:35:08 +0700

    minhthanhtr

---
+++ view/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dự án mẫu</title>
+    <link rel="stylesheet" href="view/css/style.css">
+    <link rel="stylesheet" href="view/css/danhsach_loaihang.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row mb header">
+            <h1>Siêu thị trực tuyến</h1>
+        </div>
+        <div class="row mb menu">
+            <ul>
+                <li><a href="index.php">Trang chủ</a></li>
+                <li><a href="index.php?act=gioithieu">Giới thiệu</a></li>
+                <li><a href="index.php?act=lienhe">Liên hệ</a></li>
+                <li><a href="index.php?act=goiy">Gợi ý</a></li>
+                <li><a href="index.php?act=hoidap">Hỏi đáp</a></li>
+            </ul>
+        </div>
